b6194d308d27b88b6766c15970f08310bad6ad84
Tariqul Islam <tariq10043@gmail.com>
2021-09-12 11:11:10 +0600

    author, authors and books is completed

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,7 @@
 const graphql = require('graphql')
 const _ = require('lodash')
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID} = graphql
+const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt} = graphql
 
 //dummy data
 const books = [
@@ -22,6 +22,24 @@ const books = [
     }
 ]
 
+const authors = [
+    {
+        name: "Author 1",
+        age: 44,
+        id: '1'
+    },
+    {
+        name: "Author 2",
+        age: 42,
+        id: '2'
+    },
+    {
+        name: "Author 3",
+        age: 66,
+        id: '3'
+    }
+]
+
 const BookType = new GraphQLObjectType(
     {
         name: 'Book',
@@ -29,6 +47,17 @@ const BookType = new GraphQLObjectType(
             id: {type: GraphQLID},
             name: {type: GraphQLString},
             author: {type: GraphQLString}
+        })
+    }
+)
+
+const AuthorType = new GraphQLObjectType(
+    {
+        name: 'Author',
+        fields: () => ({
+            id: {type: GraphQLID},
+            name: {type: GraphQLString},
+            age: {type: GraphQLInt}
         })
     }
 )
@@ -53,7 +82,26 @@ const RootQuery = new GraphQLObjectType({
                 //code to get data from db or source
                 return _.find(books, {id: args.id})
             }
-        }
+        },
+        books:{
+            type: new graphql.GraphQLList(BookType),
+            resolve(parent, args){
+                return books
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: {type: GraphQLID} },
+            resolve(parent, args){
+                return _.find(authors, {id: args.id})
+            }
+        },
+        authors:{
+            type: new graphql.GraphQLList(AuthorType),
+            resolve(parent, args){
+                return authors
+            }
+        },
     }
 })
 
